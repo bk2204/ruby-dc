eb49328d3fcc28bee97f105a5dba774bb0bb1711
brian m. carlson <sandals@crustytoothpaste.net>
2015-02-01 20:47:43 +0000

    Implement registers.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- lib/dc/calculator.rb
+++ lib/dc/calculator.rb
@@ -2,38 +2,58 @@ module DC
   class Calculator
     def initialize(input = $stdin, output = $stdout)
       @stack = []
+      @registers = []
+      256.times { @registers.push [] }
       @input = input
       @output = output
     end
 
-    def dispatch(op)
+    def dispatch(op, arg = nil)
       case
       when [:+, :-, :*, :/, :%].include?(op)
         binop op
       when op == :p
-        @output.puts @stack[-1].to_i
+        @output.puts @stack[0].to_i
+      when [:L, :S, :l, :s].include?(op)
+        regop op, arg
+      end
+    end
+
+    def regop(op, reg)
+      case op
+      when :L
+        @stack.unshift @registers[reg].shift
+      when :S
+        @registers[reg].unshift @stack.shift
+      when :l
+        @stack.unshift @registers[reg][0]
+      when :s
+        @registers[reg][0] = @stack.shift
       end
     end
 
     def binop(op)
-      top = @stack.pop
-      second = @stack.pop
-      @stack.push(second.send(op, top))
+      top = @stack.shift
+      second = @stack.shift
+      @stack.unshift(second.send(op, top))
     end
 
     def push(val)
-      @stack.push(val)
+      @stack.unshift(val)
     end
 
     def parse(line)
+      line.force_encoding('BINARY')
       while !line.empty?
         if line.sub!(/^(_)?(\d+(?:\.\d+)?)/, '')
           val = Rational($~[2])
           val = -val if !!$~[1]
           push(val)
         elsif line.sub!(/^\s+/, '')
-        elsif line.sub!(%r([-+*/%p]), '')
+        elsif line.sub!(%r(^[-+*/%p]), '')
           dispatch($~[0].to_sym)
+        elsif line.sub!(/^([SsLl])(.)/, '')
+          dispatch($~[1].to_sym, $~[2].ord)
         elsif line[0] == 'q'
           raise SystemExit
         else
---
+++ spec/registers.rb
@@ -0,0 +1,35 @@
+require 'stringio'
+
+require_relative '../lib/dc/calculator'
+
+describe DC::Calculator do
+  before :each do
+    @output = StringIO.new('', 'w')
+    @input = StringIO.new('', 'r')
+    @calc = DC::Calculator.new(@input, @output)
+  end
+
+  it "should be able to store a number in a register" do
+    expect { @calc.parse('2 sr') }.not_to raise_exception
+  end
+
+  it "should be able to load a number in a register" do
+    @calc.parse('2 sr lrp')
+    expect(@output.string).to eq "2\n"
+  end
+
+  it "should always load from the top with l" do
+    @calc.parse('2 Sr 3 Sr lrp lrp')
+    expect(@output.string).to eq "3\n3\n"
+  end
+
+  it "should be able to push and pop multiple numbers in a register" do
+    @calc.parse('2 Sr 3 Sr Lrp Lrp')
+    expect(@output.string).to eq "3\n2\n"
+  end
+
+  it "should have independent registers" do
+    @calc.parse('2 Sa 3 Sb Lap')
+    expect(@output.string).to eq "2\n"
+  end
+end
