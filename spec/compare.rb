5936a1667331cdd33616fc673871541f147d99ae
brian m. carlson <sandals@crustytoothpaste.net>
2015-02-04 23:20:56 +0000

    Refactor comparison tests to reduce duplication.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/compare.rb
+++ spec/compare.rb
@@ -7,65 +7,69 @@ describe DC::Calculator do
     @output = StringIO.new('', 'w+')
     @input = StringIO.new('', 'r')
     @calc = DC::Calculator.new(@input, @output)
+    @macro = '7 [20]sa'
+    # Values if the comparison is true or false, respectively.
+    @trueval = [20, 7]
+    @falseval = [7]
   end
 
   it "should not think 1 > 2" do
-    @calc.parse('7 [20]sa 1 2 >a')
-    expect(@calc.stack).to eq [7]
+    @calc.parse("#{@macro} 1 2 >a")
+    expect(@calc.stack).to eq @falseval
   end
 
   it "should think 2 > 1" do
-    @calc.parse('7 [20]sa 2 1 >a')
-    expect(@calc.stack).to eq [20, 7]
+    @calc.parse("#{@macro} 2 1 >a")
+    expect(@calc.stack).to eq @trueval
   end
 
   it "should not think 1 !> 2" do
-    @calc.parse('7 [20]sa 1 2 !>a')
-    expect(@calc.stack).to eq [20, 7]
+    @calc.parse("#{@macro} 1 2 !>a")
+    expect(@calc.stack).to eq @trueval
   end
 
   it "should think 2 !> 1" do
-    @calc.parse('7 [20]sa 2 1 !>a')
-    expect(@calc.stack).to eq [7]
+    @calc.parse("#{@macro} 2 1 !>a")
+    expect(@calc.stack).to eq @falseval
   end
 
   it "should think 1 < 2" do
-    @calc.parse('7 [20]sa 1 2 <a')
-    expect(@calc.stack).to eq [20, 7]
+    @calc.parse("#{@macro} 1 2 <a")
+    expect(@calc.stack).to eq @trueval
   end
 
   it "should not think 2 < 1" do
-    @calc.parse('7 [20]sa 2 1 <a')
-    expect(@calc.stack).to eq [7]
+    @calc.parse("#{@macro} 2 1 <a")
+    expect(@calc.stack).to eq @falseval
   end
 
   it "should not think 1 !< 2" do
-    @calc.parse('7 [20]sa 1 2 !<a')
-    expect(@calc.stack).to eq [7]
+    @calc.parse("#{@macro} 1 2 !<a")
+    expect(@calc.stack).to eq @falseval
   end
 
   it "should think 2 !< 1" do
-    @calc.parse('7 [20]sa 2 1 !<a')
-    expect(@calc.stack).to eq [20, 7]
+    @calc.parse("#{@macro} 2 1 !<a")
+    expect(@calc.stack).to eq @trueval
   end
 
   it "should not think 1 = 2" do
-    @calc.parse('7 [20]sa 1 2 =a')
-    expect(@calc.stack).to eq [7]
+    @calc.parse("#{@macro} 1 2 =a")
+    expect(@calc.stack).to eq @falseval
   end
 
   it "should think 1 = 1" do
-    @calc.parse('7 [20]sa 1 1 =a')
-    expect(@calc.stack).to eq [20, 7]
+    @calc.parse("#{@macro} 1 1 =a")
+    expect(@calc.stack).to eq @trueval
   end
 
   it "should think 1 != 2" do
-    @calc.parse('7 [20]sa 1 2 !=a')
-    expect(@calc.stack).to eq [20, 7]
+    @calc.parse("#{@macro} 1 2 !=a")
+    expect(@calc.stack).to eq @trueval
   end
 
   it "should not think 1 != 1" do
-    @calc.parse('7 [20]sa 1 1 !=a')
-    expect(@calc.stack).to eq [7]
+    @calc.parse("#{@macro} 1 1 !=a")
+    expect(@calc.stack).to eq @falseval
   end
 end
