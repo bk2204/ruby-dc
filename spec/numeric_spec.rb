c9b327ba46a7a48498c9a8492e072ebfa02bf542
brian m. carlson <sandals@crustytoothpaste.net>
2015-03-22 22:44:11 +0000

    Test Numeric conversion to Float.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/numeric_spec.rb
+++ spec/numeric_spec.rb
@@ -16,4 +16,12 @@ describe DC::Numeric do
       expect(n.to_i).to eq value.to_i
     end
   end
+
+  it 'should convert values to Floats with to_f' do
+    [1, Rational(1, 2), 0.4].each do |value|
+      n = DC::Numeric.new(value, 4, 4)
+      expect(n.to_f).to be_a Float
+      expect(n.to_f).to eq value.to_f
+    end
+  end
 end
